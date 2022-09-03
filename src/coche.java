2caed3bff09044d6b33e53520213e4de5db1f1b3
Hristo Oviedo <hristo.oviedohn@gmail.com>
2022-09-02 22:40:59 -0600

    Clase coche completado

--- src/coche.java
+++ src/coche.java
@@ -1,2 +1,10 @@
 public class coche {
+    int cantPuertas = 0;
+    public void agregarPuerta(){
+        cantPuertas++;
+    }
+
+    public void verCantPuertas(){
+        System.out.println("El coche tiene "+cantPuertas+" puertas");
+    }
 }
